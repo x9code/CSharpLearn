53f07a9088f1f86aebb164f9d9c33ffe3218ae3a
Deepak Rana <Deepak.Rana@philips.com>
2026-04-27 15:17:18 +0530

    check palindrome

---
+++ CSharpLearn/Problems/Palindrome.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpLearn.Problems
+{
+    internal class Palindrome
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("--Check Plaindrome--");
+            Console.WriteLine("Enter the word");
+            string word = Console.ReadLine();
+            CheckPalindrome(word);
+        }
+        public static void CheckPalindrome(string sen)
+        {
+            string sb = new string(sen.Reverse().ToArray());
+            if (sb.Equals(sen))
+            {
+                Console.WriteLine($"{sen} is a palindrome");
+            }
+            else
+            {
+                Console.WriteLine($"{sen} is not a palindrome");
+            }
+        }
+    }
+}
---
+++ CSharpLearn/Problems/PrimeNumbers.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpLearn.Problems
+{
+    internal class PrimeNumbers
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("-Check if the number is prime-");
+            Console.WriteLine("Enter the number");
+            int num = int.Parse(Console.ReadLine());
+            CheckPrime(num);
+        }
+        public static void CheckPrime(int n)
+        {
+            
+            int count = 0;
+            if(n == 0)
+            {
+                Console.WriteLine("u entered 0");
+            }
+            for (int i = 1; i<= n; i++)
+            {
+                if(n % i == 0)
+                {
+                    count++;
+                   
+                }
+            }
+            if( count > 2 )
+            {
+                Console.WriteLine($"{n} is not a prime number");
+            }
+            else
+            {
+                Console.WriteLine($"{n} is a prime number");
+            }
+
+        }
+    }
+}
