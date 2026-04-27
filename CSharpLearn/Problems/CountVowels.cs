9f6c3b85dbc24c8376b890c6a9f58cd67c14eced
Deepak Rana <Deepak.Rana@philips.com>
2026-04-27 11:33:22 +0530

    vowel count

--- CSharpLearn/Problems/CountVowels.cs
+++ CSharpLearn/Problems/CountVowels.cs
@@ -10,7 +10,7 @@ namespace CSharpLearn.Problems
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("enter a word");
+            Console.WriteLine("Enter a word");
             string word = Console.ReadLine();
             VowelCounter(word);
         }
@@ -18,14 +18,16 @@ namespace CSharpLearn.Problems
         {
             string str = word.ToLower();
             int aCount = 0, eCount = 0, iCount = 0, oCount = 0, uCount = 0;
-            for (int i = 0; i < str.Length; i++) {
+            for (int i = 0; i < str.Length; i++)
+            {
                 char ch = str[i];
-                switch (ch) {
-                    case 'a':aCount++; break;
-                    case 'e':eCount++; break;
-                    case 'i':iCount++; break;
-                    case 'o':oCount++; break;
-                    case 'u':uCount++; break;
+                switch(ch)
+                {
+                    case 'a':aCount++;break;
+                    case 'e':eCount++;break;
+                    case 'i':iCount++;break;
+                    case 'o':oCount++;break;
+                    case 'u':uCount++;break;
                 }
             }
             Console.WriteLine("Vowel Frequency :");
