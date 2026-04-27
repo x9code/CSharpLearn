345b972218d61511e124f0c2f9a061da7d856639
Deepak Rana <Deepak.Rana@philips.com>
2026-04-27 11:56:22 +0530

    multiplication table

---
+++ CSharpLearn/Problems/MultiplicationTable.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpLearn.Problems
+{
+    internal class MultiplicationTable
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number for multiplication table: ");
+            int num = int.Parse(Console.ReadLine());
+            MultiplicationTab(num);
+        }
+        public static void MultiplicationTab(int num)
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine($"{num} {"X"} {i} = {num*i}");
+            }
+        } 
+    }
+
+}
