09767111a137be9eb6a68d5efcf419c2c6c7702b
Deepak Rana <Deepak.Rana@philips.com>
2026-05-04 10:02:27 +0530

    CSharp Programs

---
+++ CSharpLearn/Problems/CommonPrefix.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpLearn.Problems
+{
+    internal class CommonPrefix
+    {
+        static void Main(string[] args)
+        {
+            string[] str = ["flower", "flow", "flight"];
+            string newstr =  LongestCommonPrefix(str);
+            Console.WriteLine(newstr);
+        }
+        public static string LongestCommonPrefix(string[] strs)
+        {
+            string newstr = "";
+            for (int i = 0; i < strs[0].Length; i++)
+            {
+                for (int j = i; j < strs[1].Length; j++)
+                {
+                    for (int k = i; k < strs[2].Length; k++)
+                    {
+                        if (strs[0][i] == strs[1][j] && strs[0][i] == strs[2][k])
+                        {
+                            newstr+=strs[0][i];
+                        }
+                        break;
+                    }
+                    break;
+                }
+            }
+            return newstr;
+        }
+    }
+}
